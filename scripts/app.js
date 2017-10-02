247a0657502d94d48e20c6d144e68283d413e04b
Shpetim Haxhiu <shpetim.h@gmail.com>
2017-10-02 14:46:08 +0100

    Code reorganization

---
+++ scripts/app.js
@@ -0,0 +1,35 @@
+
+  // listen to window
+
+
+  function playSound(e){
+
+    // if there is any audio tag
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    
+    if(!audio) return; //stop the function
+    audio.currentTime = 0; // rewind to start
+
+    // play the audio
+    audio.play();
+
+    // add the class to div element
+    key.classList.add('playing');
+  }
+
+  // remove trans
+  function removeTransition(e){
+    if(e.propertyName !== 'transform') return; // skip if its not a transorm
+    console.log(e);
+    this.classList.remove('playing');
+  }
+
+
+  const keys = document.querySelectorAll('.key');
+  keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+
+  // Listen for the keys
+  window.addEventListener('keydown', playSound);
